47c82da49e1c596c0faa4429e923a395a444f414
Prashant Patil <pprashant19@gmail.com>
2019-12-27 01:20:43 +0100

    Spring Boot Test project

---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,21 @@
+package com.example.springboot;
+
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class Application implements CommandLineRunner {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    log.info("args = " + Arrays.deepToString(args));
+  }
+}
